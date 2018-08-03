1d66f4ffce79c4ce5cdccd695da79c2b219134d9
Chelsea Beckton <cpb15@my.fsu.edu>
2018-08-03 23:36:18 +0000

    Lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=c710493bed1529d9199e927bb7ccf190',
                 data: query
             })
             
@@ -57,7 +57,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
-                
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -70,9 +70,9 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#release_date"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/               
-
-
+                 *********************************Insert Code Below********************************************/    
+                 
+                 $("#release_date").html(json.results[0].release_date);
 
 
                  /***********************Retrieve Movie Popularity Rate from Server & Add to HTML Document******
@@ -86,8 +86,8 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/   
-
-  
+                 
+                 $("#popularity").html(json.results[0].popularity);
 
                 
                  /*****************Retrieve Movie Original Language from Server & Add to HTML Document***********
@@ -100,8 +100,9 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#original_language"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/                 
-
+                 *********************************Insert Code Below********************************************/   
+                 
+                 $("#original_language").html(json.results[0].original_language);
 
 
                 //The following code retrieves the movie poster path from the JSON object retrieved from the TMDB 
--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(August 2, 2019)
 
 ### Student Name:  
-(First and Last Name)
+(Chelsea Beckton)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(In this assignment we uitlized a combination of Ajax and JSON in order to create an interactive movie search engine that pulls up information from the Movie Database utilizig the API key that links the information to the applicaiton in Cloud9.)
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+(https://cbeckton.github.io/lesson8_ajax/)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. (In this assignment we learned about an API key. It is a code that is used within a program's code in order to call information from the API. The key tracks and controls how the API is being used, for example, if the information was being abused it could be tracked and stopped.)
+2. (In this lesson we learned about Javascript Object Notation (JSON) which is a language that is easy to analyze and write. JSON-based API is becoming populary used by websites in order to share information with developers; websites accumalate so much information that other applications can be developed from what they've collected.)
+3. (In this assignment we learned about Bootstrap which is an open source tool used to develop HTML, CSS and jQuery applications into responsive ones. Bootstrap consists of many preset frameworks for applications.)
